90faebee3702725c0d13f6565d1993ff1e6a2235
Alaa Alzibda <alaa07996@gmail.com>
2021-08-07 15:38:48 +0300

    Update test screen

--- example/lib/screens/Texts.dart
+++ example/lib/screens/Texts.dart
@@ -1,6 +1,7 @@
-import 'package:example/utils/Constants.dart';
 import 'package:flutter/material.dart';
 import 'package:common_ui_toolkit/index.dart';
+
+import '../utils/Constants.dart';
 
 class Texts extends StatelessWidget {
   @override
@@ -21,8 +22,23 @@ class Texts extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          /// Common UI toolkit with full shadow border around the text, you can customize the containeras you like
           CommonText(
-            containerStyle: CommonContainerModel(marginHorizontal: 16.0),
+            containerStyle: CommonContainerStyle().fullShadow.copyWith(
+                  padding: 16,
+                ),
+            style: CommonTextStyles().h2Style().copyWith(
+                  fontColor: 0xFF123123,
+                ),
+            text: 'Common UI toolKit',
+          ),
+
+          /// This is an example of the verification code text with inlinSpans (Another text with different text color)
+          CommonText(
+            containerStyle: CommonContainerModel(
+              marginHorizontal: 16.0,
+              marginVertical: 16.0,
+            ),
             text:
                 'We have sent you a verification code through this number, Bla Bla Bla Bla ',
             inlineSpans: [
@@ -30,15 +46,59 @@ class Texts extends StatelessWidget {
                 child: CommonText(
                   text: '+970595131066',
                   style: CommonTextModel(fontColor: RED_COLOR),
-                  containerStyle: CommonContainerModel(marginHorizontal: 8.0),
+                  containerStyle: CommonContainerModel(
+                    marginHorizontal: 8.0,
+                    touchEffect: TouchableEffect.scaleAndFade,
+                  ),
                 ),
               ),
               // TextSpan(
               //     text: '+970595131066', style: TextStyle(color: Colors.red))
             ],
           ),
+
+          /// Simple underlined text
+          /// You can customize the underline decoration by using the TextDecoration enum
           CommonText(
-            onPress: () {},
+            text: 'Underlined text',
+            containerStyle: CommonContainerModel(
+              marginTop: 16.0,
+            ),
+            style: CommonTextModel(
+              decoration: TextDecoration.underline,
+              decorationThickness: 3.0,
+              decorationColor: RED_COLOR,
+              decorationStyle: TextDecorationStyle.dotted,
+
+              /// decorationStyle: TextDecorationStyle.wavy,
+              fontweight: FontWeight.bold,
+              fontSize: H1_FONT,
+            ),
+          ),
+
+          /// Custom container around the text with a touchable effect and a custom background color
+          CommonText(
+            containerStyle: CommonContainerModel(
+              padding: 16.0,
+              borderRaduis: 16.0,
+              marginBottom: 16.0,
+              marginVertical: 16.0,
+              backgroundColor: BLACK_COLOR,
+              touchEffect: TouchableEffect.scaleAndFade,
+            ),
+            style: CommonTextStyles().h2Style().copyWith(
+                  fontColor: WHITE_COLOR,
+                  // backgroundColor: WHITE_COLOR,
+                  // fontweight: FontWeight.bold,
+                ),
+            text: 'Simple common text',
+            onPress: () {
+              print('Simple common text pressed');
+            },
+          ),
+
+          /// Text with all side children with an icon
+          CommonText(
             style: CommonTextModel(
               iconColor: RED_COLOR,
             ),
@@ -54,16 +114,11 @@ class Texts extends StatelessWidget {
               TextSpan(text: 'icon color!'),
             ],
             containerStyle: CommonContainerModel(
-              alignment: Alignment.center,
-              backgroundColor: TRANSPARENT_COLOR,
               touchEffect: TouchableEffect.scaleAndFade,
+              marginVertical: 16.0,
             ),
             topChild: CommonText(
               text: 'Top child',
-              containerStyle: CommonContainerModel(
-                alignment: Alignment.center,
-                backgroundColor: TRANSPARENT_COLOR,
-              ),
             ),
             leftChild: CommonText(
               containerStyle: CommonContainerModel(marginHorizontal: 8.0),
@@ -72,119 +127,17 @@ class Texts extends StatelessWidget {
             rightChild: CommonText(
               containerStyle: CommonContainerModel(marginHorizontal: 8.0),
               text: 'Right child',
+              style: CommonTextModel(
+                decoration: TextDecoration.lineThrough,
+                decorationStyle: TextDecorationStyle.wavy,
+                decorationThickness: 3.0,
+              ),
             ),
             bottomChild: CommonText(
               text: 'Bottom child',
-            ),
-          ),
-          CommonText(
-            text: 'Underlined text',
-            containerStyle: CommonContainerModel(
-              marginTop: 16.0,
-            ),
-            style: CommonTextModel(
-              decoration: TextDecoration.underline,
-              decorationThickness: 3.0,
-              decorationColor: RED_COLOR,
-              decorationStyle: TextDecorationStyle.dotted,
-              fontweight: FontWeight.bold,
-              fontSize: H1_FONT,
-            ),
-          ),
-          CommonText(
-            containerStyle: CommonContainerModel(
-              padding: 16.0,
-              borderRaduis: 16.0,
-              marginBottom: 16.0,
-              backgroundColor: BLACK_COLOR,
-              touchEffect: TouchableEffect.scaleAndFade,
-            ),
-            style: CommonTextStyles().h2Style().copyWith(
-                  fontColor: WHITE_COLOR,
-                  // backgroundColor: WHITE_COLOR,
-                  // fontweight: FontWeight.bold,
-                ),
-            text: 'Simple common text',
-            onPress: () {
-              print('Simple common text pressed');
-            },
-          ),
-          CommonText(
-            containerStyle: CommonContainerModel(marginHorizontal: 16.0),
-            text:
-                'We have sent you a verification code through this number, Bla Bla Bla Bla ',
-            inlineSpans: [
-              WidgetSpan(
-                child: CommonText(
-                  text: '+970595131066',
-                  style: CommonTextModel(fontColor: RED_COLOR),
-                  containerStyle: CommonContainerModel(marginHorizontal: 8.0),
-                ),
+              style: CommonTextModel(
+                decoration: TextDecoration.underline,
               ),
-              // TextSpan(
-              //     text: '+970595131066', style: TextStyle(color: Colors.red))
-            ],
-          ),
-          CommonText(
-            onPress: () {},
-            iconPath: 'assets/icons/account_icon.svg',
-            text: 'Common text!',
-            containerStyle: CommonContainerModel(
-              alignment: Alignment.center,
-              backgroundColor: TRANSPARENT_COLOR,
-            ),
-            topChild: CommonText(
-              text: 'Top child',
-              containerStyle: CommonContainerModel(
-                alignment: Alignment.center,
-                backgroundColor: TRANSPARENT_COLOR,
-              ),
-            ),
-            leftChild: CommonText(
-              containerStyle: CommonContainerModel(marginHorizontal: 8.0),
-              text: 'Left child',
-            ),
-            rightChild: CommonText(
-              containerStyle: CommonContainerModel(marginHorizontal: 8.0),
-              text: 'Right child',
-            ),
-            bottomChild: CommonText(
-              text: 'Bottom child',
-            ),
-          ),
-          CommonText(
-            containerStyle: CommonContainerStyle().fullShadow.copyWith(
-                  padding: 16,
-                ),
-            style: CommonTextStyles().h2Style().copyWith(
-                  fontColor: 0xFF123123,
-                ),
-            text: 'Common UI toolKit',
-          ),
-          CommonText(
-            iconPath: 'assets/icons/account_icon.svg',
-            text: 'Common text!',
-            containerStyle: CommonContainerModel(
-              alignment: Alignment.center,
-              backgroundColor: TRANSPARENT_COLOR,
-            ),
-            topChild: CommonText(
-              text: 'Top child',
-              containerStyle: CommonContainerModel(
-                alignment: Alignment.center,
-                backgroundColor: TRANSPARENT_COLOR,
-              ),
-            ),
-            leftChild: CommonText(
-              containerStyle: CommonContainerModel(marginHorizontal: 8.0),
-              text: 'Left child',
-            ),
-            rightChild: CommonText(
-              containerStyle: CommonContainerModel(marginHorizontal: 8.0),
-              text: 'Right child',
-            ),
-            bottomChild: CommonText(
-              text: 'Bottom child',
             ),
           ),
         ],
