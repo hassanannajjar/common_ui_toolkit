c207423dd01da1e25947b02a2c82c118f51b58c1
Alaa Alzibda <alaa07996@gmail.com>
2021-08-30 19:26:23 +0300

    Changed initial route to home

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TextInputs(),
+      home: MyHomePage(),
       routes: {
         CONTAINERS_ROUTE: (context) => Containers(),
         TEXTS_ROUTE: (context) => Texts(),
