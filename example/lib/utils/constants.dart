ccbeb7a32c52c63b1f71c1edb84b74f63fb01758
Hassan Al-Najjar <hassan.an.najjar@gmail.com>
2022-05-16 18:42:06 +0300

    updated files name

---
+++ example/lib/utils/constants.dart
@@ -0,0 +1,9 @@
+//   **** Routes *****
+const String routeContainers = '/containers';
+const String routeTexts = '/texts';
+const String routeButtons = '/buttons';
+const String routeTextInputs = '/textInputs';
+const String routeDatePickers = '/datePicker';
+
+//   **** Colors *****
+const int colorPrimary = 0xFF9DCCD1;
